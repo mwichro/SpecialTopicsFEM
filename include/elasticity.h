6c607e1da5ff557b4f6e71cb023abad353c8d4ef
Michał Wichrowski <mtwichrowski@gmail.com>
2024-11-26 13:34:52 +0100

    class on linear elasticity

---
+++ include/elasticity.h
@@ -0,0 +1,76 @@
+#ifndef __ELASTICITY_H__
+#define __ELASTICITY_H__
+
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2024 by Luca Heltai
+//
+// This file is part of the bare-dealii-app application, based on the
+// deal.II library.
+//
+// The bare-dealii-app application is free software; you can use it,
+// redistribute it, and/or modify it under the terms of the Apache-2.0 License
+// WITH LLVM-exception as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md
+// at the top level of the bare-dealii-app distribution.
+//
+// ---------------------------------------------------------------------
+
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <fstream>
+
+using namespace dealii;
+
+template <int dim>
+class Elasticity
+{
+public:
+  Elasticity(Triangulation<dim> &tria, const unsigned int degree);
+
+  void
+  vmult(Vector<double> &dst, const Vector<double> &src) const;
+
+  void
+  intinlize();
+
+private:
+  void
+  setup_system();
+  void
+  assemble_system();
+  void
+  solve();
+  void
+  refine_grid();
+  void
+  output_results(const unsigned int cycle) const;
+
+  Triangulation<dim> &triangulation;
+
+  FESystem<dim>   fe;
+  DoFHandler<dim> dof_handler;
+
+
+  AffineConstraints<double> constraints;
+
+  SparseMatrix<double> system_matrix;
+  SparsityPattern      sparsity_pattern;
+
+  Vector<double> solution;
+  Vector<double> system_rhs;
+};
+
+#endif // __ELASTICITY_H__
